b772651a48dc32eee4f15bda67af997cf7961291
nmreadelf <frank.q.kong@hotmail.com>
2024-04-02 08:30:15 +0800

    Add: quciksort by filter

--- other/QuickSort.hs
+++ other/QuickSort.hs
@@ -1,5 +1,6 @@
 module QuickSort
 ( quickSort
+, quickSort'
 ) where 
 
 quickSort :: (Ord a) => [a] -> [a]
@@ -7,4 +8,17 @@ quickSort [] = []
 quickSort (x:xs) =
   let smallerSorted = quickSort [ a | a <- xs, a <= x ]
       biggerSorted  = quickSort [ a | a <- xs, a > x] 
+  in smallerSorted ++ [x] ++ biggerSorted
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+  | p x       = x : filter' p xs
+  | otherwise = filter p xs
+
+quickSort' :: (Ord a) => [a] -> [a]
+quickSort' [] = []
+quickSort' (x:xs) =
+  let smallerSorted = quickSort' (filter' (<=x) xs)
+      biggerSorted  = quickSort' (filter' (>x) xs)
   in smallerSorted ++ [x] ++ biggerSorted
--- other/test/QuickSortTest.hs
+++ other/test/QuickSortTest.hs
@@ -6,8 +6,10 @@ import Test.Hspec
 spec :: Spec
 spec =
   describe "QuickSort" $ do
-      it "QuickSort' test should be [1,2,3,4,5,6]" $ do
+      it "QuickSort test should be [1,2,3,4,5,6]" $ do
         QuickSort.quickSort [6,5,4,3,2,1] `shouldBe` [1,2,3,4,5,6]
+      it "QuickSort' test should be [1,2,3,4,5,6]" $ do
+        QuickSort.quickSort' [6,5,4,3,2,1] `shouldBe` [1,2,3,4,5,6]
 
 main :: IO ()
 main = hspec spec
